b8a26144150214195aae28ca62c063801f3d3cee
shivaniProgrammer2882 <kumaris35118@gmail.com>
2026-06-07 12:50:00 +0530

    Create 1_class_My_self.c

---
+++ 1_class_My_self.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("My name is shivani .\n");
+    printf("I study in class 9th .\n");
+    printf("my favourite colour is balck .\n");
+    printf("I am 14 years old .\n");
+    printf("My hobby is dance .\n");
+    printf("My school name is sarvodya kanya vidhalya .\n");                                                              
+}                                 
