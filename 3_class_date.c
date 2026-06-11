1c381c6fb3d9a43551e8c0fe13364120706ceca4
shivaniProgrammer2882 <kumaris35118@gmail.com>
2026-06-11 12:01:25 +0530

    Create 3_class_date.c

---
+++ 3_class_date.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int year = 2026;
+    int month = 6;
+    int date = 11;
+    printf("%i /0%i/%i \n",date, month,year);
+    printf("%i/0%i/%i\n",year,month,date);
+}
