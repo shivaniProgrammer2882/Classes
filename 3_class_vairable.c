df2c24eace4888fcf85ad45e93c5ae82f8fc7bac
shivaniProgrammer2882 <kumaris35118@gmail.com>
2026-06-12 22:35:25 +0530

    Create 3_class_vairable.c

---
+++ 3_class_vairable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int date = 12 ;
+    int year =2026 ;
+    printf("today is %ith june %i \n",date,year);
+        printf("today is %ith june %i \n",date,year);
+            printf("today is %ith june %i \n",date,year);
+                printf("today is %ith june %i \n",date,year);
+                    printf("today is %ith june %i \n",date,year);
+                        printf("today is %ith june %i \n",date,year);
+                            printf("today is %ith june %i \n",date,year);
+                                printf("today is %ith june %i \n",date,year);
+                                    printf("today is %ith june %i \n",date,year);
+                                        printf("today is %ith june %i \n",date,year);  
+ }
