92aaadf35bd859bd9937fa071317e2f20a30729c
shivaniProgrammer2882 <kumaris35118@gmail.com>
2026-06-16 18:01:05 +0530

    Create 5_class_comments.c

---
+++ 5_class_comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //declaring an int variable name date    
+    int date = 16;
+    //declaring an int variable name month
+    int month = 6;
+    //declaring an int variablename year
+    int year = 2026;
+    //printing date in this line
+    printf("%i/0%i/%i\n",date,month,year);
+ 
+ }               
