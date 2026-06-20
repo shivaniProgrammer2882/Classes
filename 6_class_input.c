7a93fa0d80fd8cc5a330021ac3edfba8c1046828
shivaniProgrammer2882 <kumaris35118@gmail.com>
2026-06-20 12:18:04 +0530

    Create 6_class_input.c

---
+++ 6_class_input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //decalring int variable
+    int num;
+    printf(" enter the number:");
+    //taking input for num variable
+    scanf("%i", &num);
+    //declaring the value of num 
+    printf("Number is: %i\n",num);
+ 
+ }               
