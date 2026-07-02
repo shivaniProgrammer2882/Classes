f7bb9b86e1fc42db1ec84b5a32b506354f8f36b9
shivaniProgrammer2882 <kumaris35118@gmail.com>
2026-07-02 17:28:11 +0530

    Create 9_class_long_ int.c

---
+++ 9_class_long_ int.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (void)
+{
+   long int my_phone_no =7763913178;
+   long int papa_phone_no = 9015430818;
+   long int didi_phone_no = 9953370883;
+   long int bhaiya_phone_no = 9821408821;   printf("my_phone_no = %li\n",my_phone_no);
+   printf("papa_phone_no = %li\n",papa_phone_no);
+   printf("didi_phone_no = %li\n",didi_phone_no);
+   printf("bhaiya_phone_no = %li\n",bhaiya_phone_no);
+}     
